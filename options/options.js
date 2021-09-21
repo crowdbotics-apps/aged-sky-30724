48923217bbece08181a240013ac8ec50fa437bce
Crowdbotics <team@crowdbotics.com>
2021-09-21 15:53:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "aged_sky_30724Identifier",
+  url: "https://aged_sky_30724Identifier.botics.co",
+  api: "https://aged_sky_30724Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
